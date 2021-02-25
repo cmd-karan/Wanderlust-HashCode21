f20f34c10b73752ae36db552d7c6d78f22300711
Prateek Gupta <prategupta@paypal.com>
2021-02-26 00:30:36 +0530

    input format

--- main/main.py
+++ main/main.py
@@ -1,3 +1,18 @@
+"""
+    edgeName: [startNode, endNode]
+    (startNode, endNode): edgeName
+    carID: [edgeNames],[nodeNames]
+
+"""
+
 if __name__=="__main__":
     print("main called maybe")
-    
+    path = ""
+    simulationTime, intersectionCount, streetCount, scorePerCar, graph, edgeToNode, nodeToEdge, carIDMap = inputFile.InputFile.getInput(path)
+    #adJList
+    #graph[endNode].append(startNode)
+
+        
+        
+
+
