7665c8855816b96cbab3cf78232f6f6d9cfa2db7
Prateek Gupta <prategupta@paypal.com>
2021-02-26 00:51:25 +0530

    bruteForce 1

--- main/main.py
+++ main/main.py
@@ -4,13 +4,34 @@
     carID: [edgeNames],[nodeNames]
 
 """
+"""
+Get all the files 
+get the input
+comput the logic
+get the output format
+create a output file
+and save it
+#adJList
+    #graph[endNode].append(startNode)
+"""
+import os
 
 if __name__=="__main__":
     print("main called maybe")
-    path = ""
-    simulationTime, intersectionCount, streetCount, scorePerCar, graph, edgeToNode, nodeToEdge, carIDMap = inputFile.InputFile.getInput(path)
-    #adJList
-    #graph[endNode].append(startNode)
+    currDir = os.getcwd()
+    
+    inputDir = os.path.join(currDir, "dataset")
+    outputDir = os.path.join(currDir, "output")
+    fileNames = list(os.listdir(inputDir))
+    for fileName in fileNames:
+        path = os.path.join(inputDir, fileName)
+        simulationTime, intersectionCount, streetCount, scorePerCar, graph, edgeToNode, nodeToEdge, carIDMap = inputFile.InputFile.getInput(path)
+        s = outputFile.OutputFile.getOutput(simulationTime, intersectionCount, streetCount, scorePerCar, graph, edgeToNode, nodeToEdge, carIDMap)
+        outputFileName = os.path.join(outputDir,fileName+"_output.txt")
+        with open(outputFileName, 'w') as file:
+            file.write(s)
+    
+        
 
         
         
---
+++ main/outputFile.py
@@ -0,0 +1,38 @@
+class OutputFile:
+    
+    def getOutput(self, simulationTime, intersectionCount, streetCount, scorePerCar, graph, edgeToNode, nodeToEdge, carIDMap):
+        return self.getOutputv1(simulationTime, intersectionCount, streetCount, scorePerCar, graph, edgeToNode, nodeToEdge, carIDMap,fileName, path)
+
+    def streetWeight(self):
+        return 1
+    def getOutputv1(self, simulationTime, intersectionCount, streetCount, scorePerCar, graph, edgeToNode, nodeToEdge, carIDMap):
+        """
+        Node 
+        startNodeCount
+        for startNode in graph[endNode]:
+            streetName -startNode, endNode
+            T = 1 (always)
+
+        node number is from 0 to N-1
+
+        Output Format:
+        3
+        1
+        2
+        rue-d-athenes 2 rue-d-amsterdam 1 0
+        1 rue-de-londres 2
+        2
+        1 rue-de-moscou 1
+        """
+        res = [str(intersectionCount)]
+
+        for endNode in range(intersectionCount):
+            res.append(endNode)
+            res.append(len(graph[endNode]))
+            for startNode in graph[endNode]:
+                streetName = nodeToEdge[(startNode, endNode)]
+                res.append(streetName+self.streetWeight())
+
+        return "\n".join([str(elem) for elem in res])
+        
+        
